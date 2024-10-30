7a14b208a841222cd033adbe7da48cd128ddc8b2
Nathan <nathanhurst03@gmail.com>
2024-10-30 07:40:59 -0600

    Some re-organization

---
+++ arson/src/core/mod.rs
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+
+mod error;
+mod symbol;
+mod node;
+
+pub use error::*;
+pub use symbol::*;
+pub use node::*;
--- arson/src/lib.rs
+++ arson/src/lib.rs
@@ -1,9 +1,5 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
 
-mod error;
-mod symbol;
-mod value;
+mod core;
 
-pub use error::*;
-pub use symbol::*;
-pub use value::*;
+pub use core::*;
