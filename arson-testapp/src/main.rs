53390fa079f9dba6c59641d3f0981a500ae04f04
Nathan <nathanhurst03@gmail.com>
2024-11-02 17:36:21 -0600

    Test app proof-of-concept

--- arson-testapp/src/main.rs
+++ arson-testapp/src/main.rs
@@ -1,5 +1,21 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
 
-fn main() {
-    println!("Hello, world!");
+use arson::{Context, Node};
+
+fn main() -> arson::Result<()> {
+    println!("Hello from native!");
+
+    let mut context = Context::new();
+
+    context.register_func_by_name("print", |_context, args| {
+        let message = args.string(1)?;
+        println!("{message}");
+        Ok(Node::Integer(0))
+    })?;
+
+    let file = context.load_text(include_str!("../run/main.dta"))?;
+    let command = file.find_array(&context.add_symbol("main"))?.command(1)?;
+    context.execute(&command)?;
+
+    Ok(())
 }
