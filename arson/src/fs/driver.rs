a239e3e4b1001cc6dc275f1864d98a903ed2a33f
Nathan <nathanhurst03@gmail.com>
2024-11-17 19:03:13 -0700

    Add a basic file system driver

--- arson/src/fs/driver.rs
+++ arson/src/fs/driver.rs
@@ -1,8 +1,9 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
 
 use std::{
+    fs::File,
     io::{self, Read, Write},
-    path::PathBuf,
+    path::{Path, PathBuf},
 };
 
 use super::AbsolutePath;
@@ -59,4 +60,81 @@ pub trait FileSystemDriver {
     /// The file otherwise has standard read permissions, and this function exists
     /// primarily to allow for additional filtering where desired.
     fn open_execute(&self, path: &AbsolutePath) -> io::Result<Box<dyn Read>>;
+}
+
+/// An extremely basic file system driver which simply provides raw access to a specific directory.
+///
+/// Because [`AbsolutePath`] and [`FileSystem`](super::FileSystem) handle path sandboxing inherently,
+/// this driver prevents sandbox escapes naturally. To doubly ensure this, a paranoid safety assert
+/// is done when resolving the virtual path to the on-disk one to ensure no parent directory components
+/// slip through.
+pub struct BasicFileSystemDriver {
+    mount_dir: PathBuf,
+}
+
+impl BasicFileSystemDriver {
+    /// Creates a new [`BasicFileSystemDriver`] with the given mount directory.
+    ///
+    /// # Errors
+    ///
+    /// Returns an error if the given path could not be canonicalized or is not a directory.
+    pub fn new(mount_dir: &Path) -> io::Result<Self> {
+        let mount_dir = mount_dir.canonicalize()?;
+        if !mount_dir.is_dir() {
+            // FIXME: Use ErrorKind::NotADirectory when that stabilizes
+            return Err(io::Error::new(io::ErrorKind::InvalidInput, "not a directory"));
+        }
+
+        Ok(Self { mount_dir })
+    }
+
+    fn resolve_path(&self, path: &AbsolutePath) -> PathBuf {
+        assert!(
+            !path.as_str().contains(".."),
+            "attempted to resolve non-canonicalized virtual path"
+        );
+        self.mount_dir.join(path.as_str())
+    }
+}
+
+impl FileSystemDriver for BasicFileSystemDriver {
+    fn exists(&self, path: &AbsolutePath) -> bool {
+        self.resolve_path(path).exists()
+    }
+
+    fn is_file(&self, path: &AbsolutePath) -> bool {
+        self.resolve_path(path).is_file()
+    }
+
+    fn is_dir(&self, path: &AbsolutePath) -> bool {
+        self.resolve_path(path).is_dir()
+    }
+
+    fn try_exists(&self, path: &AbsolutePath) -> io::Result<bool> {
+        self.resolve_path(path).try_exists()
+    }
+
+    fn try_is_file(&self, path: &AbsolutePath) -> io::Result<bool> {
+        self.resolve_path(path).metadata().map(|m| m.is_file())
+    }
+
+    fn try_is_dir(&self, path: &AbsolutePath) -> io::Result<bool> {
+        self.resolve_path(path).metadata().map(|m| m.is_dir())
+    }
+
+    fn create(&self, path: &AbsolutePath) -> io::Result<Box<dyn Write>> {
+        File::create(&self.resolve_path(path)).map::<Box<dyn Write>, _>(|f| Box::new(f))
+    }
+
+    fn create_new(&self, path: &AbsolutePath) -> io::Result<Box<dyn ReadWrite>> {
+        File::create_new(&self.resolve_path(path)).map::<Box<dyn ReadWrite>, _>(|f| Box::new(f))
+    }
+
+    fn open(&self, path: &AbsolutePath) -> io::Result<Box<dyn Read>> {
+        File::open(&self.resolve_path(path)).map::<Box<dyn Read>, _>(|f| Box::new(f))
+    }
+
+    fn open_execute(&self, path: &AbsolutePath) -> io::Result<Box<dyn Read>> {
+        self.open(path)
+    }
 }
