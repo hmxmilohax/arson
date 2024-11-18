1fe5c9843d4b8d79930465e30736199d0e8de09c
Nathan <nathanhurst03@gmail.com>
2024-11-17 19:55:01 -0700

    Hook up file system to testapp, fix runtime errors

--- arson-testapp/src/main.rs
+++ arson-testapp/src/main.rs
@@ -1,22 +1,31 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
 
+use std::path::Path;
+
+use arson::fs::BasicFileSystemDriver;
 use arson::{Context, LoadOptions, NodeValue};
 
 fn main() -> arson::Result<()> {
-    println!("Hello from native!");
+    println!("> Hello from native!");
 
-    let mut context = Context::new();
+    let mount_dir = Path::new(file!()).join("../../run");
+    let driver = BasicFileSystemDriver::new(&mount_dir)?;
+    let mut context = Context::with_file_driver(driver);
+    println!("Created context.");
 
     context.register_func_by_name("print", |context, args| {
-        let message = args.string(context, 1)?;
-        println!("{message}");
+        let message = args.string(context, 0)?;
+        println!("> {message}");
         Ok(NodeValue::HANDLED)
     });
 
     let options = LoadOptions { allow_include: true };
-    let file = context.load_text(options, include_str!("../run/main.dta"))?;
+    let file = context.load_path(options, "/main.dta")?;
+    println!("Loaded main.dta");
+
     let command = file.find_array(&context.add_symbol("main"))?.command(1)?;
     context.execute(&command)?;
+    println!("Ran main.dta!");
 
     Ok(())
 }
