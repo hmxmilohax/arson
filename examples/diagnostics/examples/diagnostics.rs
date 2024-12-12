7b65f20fc2f409d1bfedb4b0bdfbe581c8e6e969
Nathan <nathanhurst03@gmail.com>
2024-12-12 00:21:23 -0700

    Only use arson-parse in diagnostics example Compile-tests having the `loading` feature disabled on arson-parse

--- examples/diagnostics/examples/diagnostics.rs
+++ examples/diagnostics/examples/diagnostics.rs
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
+use std::fs::File;
 use std::path::Path;
 
-use arson::parse::reporting as codespan_reporting;
-use arson::prolog::*;
+use arson_parse::reporting as codespan_reporting;
 use codespan_reporting::files::SimpleFile;
 use codespan_reporting::term;
 use codespan_reporting::term::termcolor::{ColorChoice, StandardStream};
@@ -11,35 +11,32 @@ use codespan_reporting::term::Chars;
 
 fn main() {
     let mount_dir = Path::new(file!()).join("../../run");
-    let driver = BasicFileSystemDriver::new(&mount_dir).expect("provided path is a directory");
-    let file_system = FileSystem::new(driver);
 
     let files = ["success.dta", "fail.dta"];
     for file in files {
         println!("Parsing file {file}");
 
-        print_parsed(&file_system, file);
+        print_parsed(&mount_dir, file);
 
         println!();
     }
 }
 
-fn print_parsed(fs: &FileSystem, path: impl AsRef<VirtualPath>) {
-    let path = path.as_ref();
+fn print_parsed(mount_dir: &Path, file: &str) {
     let text = {
-        let file = fs.open(path).expect("failed to open file");
+        let file = File::open(mount_dir.join(file)).expect("failed to open file");
         std::io::read_to_string(file).expect("failed to read file")
     };
 
-    match arson::parse::parse_text(&text) {
+    match arson_parse::parse_text(&text) {
         Ok(parsed) => {
-            println!("File {path}:");
+            println!("File {file}:");
             for expr in parsed {
                 println!("- ({:?}) {:?}", expr.location, expr.value);
             }
         },
         Err(diagnostics) => {
-            let db = SimpleFile::new(path, text);
+            let db = SimpleFile::new(file, text);
             let writer = StandardStream::stderr(ColorChoice::Auto);
             let config = codespan_reporting::term::Config { chars: Chars::ascii(), ..Default::default() };
 
