1ea1debb90e43a1fb0b75663b9014eb915e7d4dd
Nathan <nathanhurst03@gmail.com>
2025-01-28 23:48:50 -0700

    sprint, remove `> ` prefix from print

--- crates/arson-stdlib/src/stdio.rs
+++ crates/arson-stdlib/src/stdio.rs
@@ -43,12 +43,21 @@ impl std::fmt::Display for VarargFormatter<'_> {
 
 pub fn register_funcs(context: &mut Context) {
     context.register_func("print", self::print);
+    context.register_func("sprint", self::sprint);
 }
 
 pub fn print(context: &mut Context, args: &NodeSlice) -> ExecuteResult {
     if !args.is_empty() {
-        println!("> {}", VarargFormatter::new(context, args));
+        println!("{}", VarargFormatter::new(context, args));
     }
 
     Ok(Node::HANDLED)
+}
+
+pub fn sprint(context: &mut Context, args: &NodeSlice) -> ExecuteResult {
+    if !args.is_empty() {
+        Ok(VarargFormatter::new(context, args).to_string().into())
+    } else {
+        Ok(String::new().into())
+    }
 }
